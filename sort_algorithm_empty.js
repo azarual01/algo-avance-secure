59c5030fecb74c8f37ebab8b8c064ff24389fd01
El Ghazi AZARUAL <elghazi.azarual@Digital-Grenoble.local>
2023-07-04 10:06:46 +0200

    import js file

--- sort_algorithm_empty.js
+++ sort_algorithm_empty.js
@@ -1,27 +1,45 @@
 
-function swap(A, i, j) {
-    console.log("implement me");
-}
 
 function insertionSort(inputArr) {
-    console.log("implement me");
-}
+    console.log("before "+inputArr);
+    let temp,i,j;
+    for(i = 1; i < inputArr.length; i++){
 
-function selectionSort(inputArr) {
-    console.log("implement me");
+        temp = inputArr[i];
+        j = i;
+        while(j > 0 && inputArr[j-1] > temp){
+            inputArr[j] = inputArr[j-1];
+            inputArr[j-1] = temp;
+            // swap(inputArr,i,j);
+            j--;
+        }
+    }
 }
+function swap(A, i, j) {
+    let temp = A[i];
+    console.log(i,j,temp);
+    
+    A[j] =  A[j-1];
+    A[j-1] = temp;
+}
+    
 
-function bubbleSort(inputArr) {
-    console.log("implement me");
-}
 
-function shellSort(inputArr) {
-    console.log("implement me");
-}
+// function selectionSort(inputArr) {
+//     console.log("implement me");
+// }
 
-function heapSort(inputArr, indexStart, indexEnd) {
-    console.log("implement me");
-}
+// function bubbleSort(inputArr) {
+//     console.log("implement me");
+// }
+
+// function shellSort(inputArr) {
+//     console.log("implement me");
+// }
+
+// function heapSort(inputArr, indexStart, indexEnd) {
+//     console.log("implement me");
+// }
 
 /////////////////////////////////////////////////////////////
 //                          START                       ////
@@ -39,23 +57,23 @@ for (let i = 0; i < size; i++) {
 // Calculs, performances
 
 var swappedList = [...list];
-swap(swappedList, 0, 1);
+// swap(swappedList, 0, 1);
 
 var insertionList = insertionSort([...list]);
 
-var selectionList = selectionSort([...list]);
+// var selectionList = selectionSort([...list]);
 
-var bubbleList = bubbleSort([...list]);
+// var bubbleList = bubbleSort([...list]);
 
-var shellList = shellSort([...list]);
+// var shellList = shellSort([...list]);
 
-var heapList = heapSort([...list]);
+// var heapList = heapSort([...list]);
 
 // Affichage des résultats
 console.log("Liste non triee");
 console.log(list);
-// console.log("Swap des deux premiers elements");
-// console.log(swappedList);
+console.log("Swap des deux premiers elements");
+console.log(swappedList);
 // console.log("Insertion");
 // console.log(insertionList)
 // console.log("Selection");
